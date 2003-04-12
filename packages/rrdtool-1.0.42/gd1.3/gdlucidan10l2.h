a187a053d4c23488011cb6ed3f01ffd75d6c4ac1
Blair Zajac <blair@orcaware.com>
2003-04-12 17:45:54 +0000

    Update rrdtool from 1.0.41 to 1.0.42.
    * configure.in:
      Bump the required RRDs Perl VERSION number from 1.000411 to 1.000421
      and update the directory to find rrdtool in from rrdtool-1.0.41 to
      rrdtool-1.0.42.
    
    * INSTALL (Determine which Perl modules need compiling and installing):
      Bump the required RRDs Perl VERSION number from 1.000411 to 1.000421
      and update the RRDs package version from 1.0.41 to 1.0.42.
    
    * packages/rrdtool-1.0.42:
      Renamed from packages/rrdtool-1.0.41.  Directory contents updated
      from rrdtool-1.0.42.tar.gz.

---
+++ packages/rrdtool-1.0.42/gd1.3/gdlucidan10l2.h
@@ -0,0 +1,21 @@
+
+#ifndef _GDLUCIDAN10L2_H_
+#define _GDLUCIDAN10L2_H_ 1
+
+/*
+	This is a header file for gd font, generated using
+	bdftogd version 0.60 by Jan Pazdziora, adelton@fi.muni.cz
+	from bdf font
+	-B&H-LucidaTypewriter-Medium-R-Normal-Sans-10-100-75-75-M-60-iso8859-2
+	at Wed Apr  2 23:42:34 2003.
+	The original bdf was holding following copyright:
+	"Copyright Bigelow & Holmes 1986, 1985."
+ */
+
+
+#include "gd.h"
+
+extern gdFontPtr gdLucidaNormal10l2;
+
+#endif
+

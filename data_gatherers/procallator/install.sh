68b9cce4098aaa1f66b6f68caf96b82d903989eb
Blair Zajac <blair@orcaware.com>
2003-06-15 19:38:30 +0000

    Do the first major integration of procallator into the Orca configure and Makefile system.  This commit doesn't take on fixing or upgrading the procallator script itself, that'll come next.
    * INSTALL
      ([Systems with System Statistics in /proc, i.e. Linux] Install procallator):
        New section for installing procallator on a system.
    
    * Makefile.in:
      (procallator_run_at_boot): New rule to run
        "make procallator_run_at_boot" in data_gatherers/procallator.
    
    * configure.in:
      Search for chkconfig so that procallator can be installed into
        /etc/rc.d/init.d on Linux systems.
      Search for the /proc equivalent directory on the system.
      Add a new configure command line option, --enable-librrdtool, which
        forces RRDtool to be compiled even if Perl already has RRDtool
        installed, and installs librrd.* into Orca's $libdir.
      By default, the configure script will build and install all
        available data gatherers so that a single Orca installation can be
        used for multiple operating systems.  Add three new command line
        options that disable installing particular data gatherers,
        --disable-orcallator, --disable-orca-services and
        --disable-procallator.
    
    * data_gatherers/Makefile.in:
      (procallator_run_at_boot): New rule to run
        "make procallator_run_at_boot" in data_gatherers/procallator.
      Set $(SUBDIRS) to the subdirectories specified by configure.in.
    
    * data_gatherers/procallator/install.sh:
      Removed.  Functionality replaced by Makefile.in.
      
    * data_gatherers/procallator/Makefile.in:
      Copied from data_gatherers/orcallator/Makefile.in.
      s/orcallator/procallator/g.
      Use chkconfig to set up the links from
        /etc/rc.d/init.d/S99procallator into /etc/rc.d/rcX.d/.
    
    * data_gatherers/procallator/README:
      Clean up formatting.
      Remove installation section, since it is now provided by configure
        and Makefile.
      Still needs some more work.
    
    * data_gatherers/procallator/S99procallator.sh.in:
      Renamed from procallator.
      Update to look more like standard /etc/rc.d/init.d scripts.
      Do not use hardwired paths to procallator; use paths supplied by
        configure.
      (start):
        Create /var/lock/subsys/procallator and /var/run/procallator.pid.
      (stop)
        Remove /var/lock/subsys/procallator and /var/run/procallator.pid.
        Use killproc() instead of grep to find procallator processes.
      (status):
        New subcommand.
      (restart):
        New subcommand.
    
    * data_gatherers/procallator/procallator.cfg.in:
      Require Orca version 0.265 or greater.
      Replace hardwired paths for base_dir and html_dir with the paths
        specified by configure.
      Turn expire_images on.
      Replace guilherme.chehab@tco.net.br's address for warn_email with
        WARN_EMAIL.  I'm sure he'll appreciate this :)
      Add the generate_*_plot parameters to generate all but the hourly
        plots.
      Use the html_page_header, html_top_title and html_page_footer from
        orcallator.cfg.in.
      Rename the group orcallator to group procallator.
      Remove the reopen option from the group procallator.  This should
        speed up processing of input data files.
      For the Average # Processes in Run Queue plot, increase data_max
        from 100 to 1000 and remove the color for the plots.
      For the Interface Input Bits Per Second and the Interface Output
        Bits Per Second plots, increase data_max to handle Gigabit
        devices.
      Make the TCP Current Connections plot use area instead of a line.
      Move the "Page Ins & Outs rate" and "Swap Ins & Outs rate" into the
        memory section of plots.
      Fix some y_legend names to be consistent, specifically change "rate"
        to "Counts/s", and use consistent capitalization.
      Change colors for some plots.

--- data_gatherers/procallator/install.sh
+++
@@ -1,8 +0,0 @@
-cp procallator.pl /usr/local/bin
-cp procallator /etc/rc.d/init.d
-ln -s /etc/rc.d/init.d/procallator /etc/rc.d/rc3.d/S99procallator
-/etc/rc.d/init.d/procallator start
-echo "Dont forget to copy procallator.cfg to your orca server, and start a new instance of orca using this file as the config file"
-echo "To copy the collected files I suggest the use of rsync"
-echo "This software is in alpha stagge, use at you own risk"
-echo "Copyright (C) 2001 Guilherme Carvalho Chehab.  All Rights Reserved" 
